2c0173e1941286c08b6a23bd56f81e5fd16bc087
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-14 09:03:24 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <h1 className='text-green-500 text-5xl'>Hello</h1>
-      <button className='btn btn-primary'>
-          button
-      </button>
+      <Navbar></Navbar>
     </div>
   );
 }
