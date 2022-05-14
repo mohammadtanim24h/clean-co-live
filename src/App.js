18eaa3c364cb636ae8f7e172e038823dab31c760
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-14 14:16:45 +0600

    animation on scroll added in landing text

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { publicRoutes } from "./routes/publicRoutes";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 function App() {
+    // Aos init
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
     return (
         <div>
             <Navbar>
--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -14,20 +14,20 @@ const Landing = () => {
                         />
                     </div>
                     <div>
-                        <p className="text-xl ms-1">Best Quality</p>
-                        <h1 class="text-5xl font-bold">Professional Cleaning Service</h1>
-                        <p class="py-6 max-w-xl">
+                        <p data-aos="fade-right" data-aos-delay="200" data-aos-duration="1000" className="text-xl ml-1">Best Quality</p>
+                        <h1 data-aos="fade-right" data-aos-delay="300" data-aos-duration="900" class="text-5xl font-bold">Professional Cleaning Service</h1>
+                        <p data-aos="fade-right" data-aos-delay="400" data-aos-duration="800" class="py-6 max-w-xl">
                             Provident cupiditate voluptatem et in. Quaerat
                             fugiat ut assumenda excepturi exercitationem quasi.
                             In deleniti eaque aut repudiandae et a id nisi.
                         </p>
-                        <button class="btn btn-primary">Get Started</button>
+                        <button data-aos="zoom-in" data-aos-delay="900" data-aos-duration="800" class="btn btn-primary">Get Started</button>
                     </div>
                 </div>
             </div>
             {/* Free Estimate */}
             <div className="shadow-lg p-10 w-3/4 mx-auto bg-base-200 mt-[-60px] relative z-99 rounded-xl">
-                <h1 className="text-3xl mb-6">Get Free Estimate</h1>
+                <h1 className="text-3xl mb-6 text-primary">Get Free Estimate</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                     <input type="text" placeholder="Type here" class="input input-bordered w-full" />
                     <input type="text" placeholder="Type here" class="input input-bordered w-full" />
@@ -38,6 +38,7 @@ const Landing = () => {
                     <input type="text" placeholder="Type here" class="input input-bordered w-full" />
                     <input type="text" placeholder="Type here" class="input input-bordered w-full" />
                 </div>
+                <button className="btn btn-primary text-white px-4 py-2 mt-5">Request a Quote</button>
             </div>
         </div>
     );
