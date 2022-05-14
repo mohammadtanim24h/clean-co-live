8c3e7c8f80f4da5f0c7127241b9b8a5772679f0b
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-14 08:42:08 +0600

    daisyui added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className='text-green-500 text-5xl'>Hello</h1>
+      <button className='btn btn-primary'>
+          button
+      </button>
     </div>
   );
 }
